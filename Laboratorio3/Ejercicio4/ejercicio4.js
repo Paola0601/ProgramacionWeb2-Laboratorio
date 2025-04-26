d4a0346dd6660e24714cf00ac406fba8a065f8e6
Paola0601 <pmaytaqu@unsa.edu.pe>
2025-04-26 11:05:50 -0500

    primer codigo para el ejercicio del java script, se mejorara mientras enontremos errores

--- Laboratorio3/Ejercicio4/ejercicio4.js
+++ Laboratorio3/Ejercicio4/ejercicio4.js
@@ -1,5 +1,16 @@
 const linkDelMeet=document.getElementById("linkMeet");
 const boton=document.getElementById("boton");
 boton.addEventListener("click", mostrarLink);
+
 function mostrarLink(){
-document.getElementById("codigo").innerHTML=linkDelMeet.value;}
+document.getElementById("codigo").innerHTML=codigoSinGuiones;}
+function sacarCodigo(){
+    link=linkDelMeet.value;
+    const partes=link.split("/");
+    const codigo=partes[partes.length-1];
+    const codigoSinGuiones=sacarGuiones(codigo);
+}
+function sacarGuiones(codigo){
+    const codigoSinGuiones=codigo.replace(/-/g,"");
+    return codigoSinGuiones;
+}
