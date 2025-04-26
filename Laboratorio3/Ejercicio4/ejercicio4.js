e6e3c7d53722b9b9eab97a095a0b0afc5d0ad5f4
Paola0601 <pmaytaqu@unsa.edu.pe>
2025-04-26 09:23:38 -0500

    Creo funciones, que resolveran el ejercicio

---
+++ Laboratorio3/Ejercicio4/ejercicio4.js
@@ -0,0 +1,17 @@
+
+    const formulario = document.getElementById("formulario");
+    const contenidoDeLaTabla = document.getElementById("tablaContainer");
+    const resultadoSuma = document.getElementById("resultado");
+
+  
+      
+    function crearTabla()
+    {};
+    function calcularSuma()
+    { };
+    function mostrarResultado()
+    { };
+    function agregarBotonSumar()
+    { };
+    function limpiarTodo()
+    { };
