0b1e22417e41b2545bb3e42cd02282e216df29c7
Paola0601 <pmaytaqu@unsa.edu.pe>
2025-04-26 11:18:42 -0500

    ya funciona pero aun no saca todo el codigo

--- Laboratorio3/Ejercicio4/ejercicio4.js
+++ Laboratorio3/Ejercicio4/ejercicio4.js
@@ -5,12 +5,13 @@ boton.addEventListener("click", mostrarCodigo);
 function mostrarCodigo(){
     const codigoFinal = sacarCodigo(); 
     document.getElementById("codigo").innerHTML = codigoFinal;
-document.getElementById("codigo").innerHTML=codigoSinGuiones;}
+}
 function sacarCodigo(){
-    link=linkDelMeet.value;
+    link=linkDelMeet.textContent;
     const partes=link.split("/");
     const codigo=partes[partes.length-1];
     const codigoSinGuiones=sacarGuiones(codigo);
+    return codigoSinGuiones;
 }
 function sacarGuiones(codigo){
     const codigoSinGuiones=codigo.replace(/-/g,"");
