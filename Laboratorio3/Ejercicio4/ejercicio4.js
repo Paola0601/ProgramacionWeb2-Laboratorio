5b542485efdd6bca70633885dcc524371f27c1d3
Paola0601 <pmaytaqu@unsa.edu.pe>
2025-04-26 09:57:15 -0500

    terminamos la funcion suma y el boton para sumar, pero este aun no funcina y la suma sale 0,falta corregir

--- Laboratorio3/Ejercicio4/ejercicio4.js
+++ Laboratorio3/Ejercicio4/ejercicio4.js
@@ -6,6 +6,7 @@
     formulario.addEventListener("submit", function(event) {
       event.preventDefault(); // Evitar el envío del formulario
       crearTabla();
+   
     });
 
   
@@ -27,15 +28,25 @@
       tabla.appendChild(fila);
       contenidoDeLaTabla.appendChild(tabla);
 
+        calcularSuma();      
     }; 
     
     
     
     function calcularSuma()
-    { };
+    { numeros=[];
+      let suma=0;
+      for(let i=0;i<numeros.length;i++)
+      {suma+=numeros[i];}
+      resultadoSuma.innerHTML="La suma es: "+suma;
+    }
+    function agregarBotonCalcularSuma()
+    { const botonCalcularSuma=document.createElement("button");
+      botonCalcularSuma.textContent="Calcular Suma";
+      botonCalcularSuma.addEventListener("click",calcularSuma);
+      contenidoDeLaTabla.appendChild(botonCalcularSuma);
+    };
     function mostrarResultado()
-    { };
-    function agregarBotonSumar()
     { };
     function limpiarTodo()
     {contenidoDeLaTabla.innerHTML="";
