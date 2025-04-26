e57635f177b0c0c7ffed65745f0fa66f8cc4b1e1
Paola0601 <pmaytaqu@unsa.edu.pe>
2025-04-25 22:16:46 -0500

    PRUEBA 1:error de la funcion invertir palabra en el bucle for

--- Laboratorio3/Ejercicio2/ejercicio2.js
+++ Laboratorio3/Ejercicio2/ejercicio2.js
@@ -2,6 +2,13 @@ document.getElementById("invertir").addEventListener("click",invertirPalabra);
 
 function invertirPalabra(){
 
+  const palabraIngresada= document.getElementById("palabraIngresada").value.trim();
+  let palabraInvertida="";
+  for(let i=0;i<palabraIngresada.length;i++)
+  {
+    palabraInvertida=palabraIngresada+palabraIngresada.charAt(i);
 
-    
+  }
+document.getElementById("palabraInvertida").innerHTML=palabraInvertida;
+
 }
