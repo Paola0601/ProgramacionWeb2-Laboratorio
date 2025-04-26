a13713446dec742475904dc3a947c10e797f7cde
Paola0601 <pmaytaqu@unsa.edu.pe>
2025-04-25 23:17:33 -0500

    implementamos el if el javaScrip para evitar errores

--- Laboratorio3/Ejercicio3/ejercicio3.js
+++ Laboratorio3/Ejercicio3/ejercicio3.js
@@ -1,6 +1,11 @@
 const fechaActual = new Date();
 const anioActual = fechaActual.getFullYear();
 let aniversarioArequipa = new Date(anioActual, 7, 15); 
+//Implementamos este if para que nos ayude a evitar eerrores en el caso de que la fecha actual sea mayor a la fecha de aniversario
+// de Arequipa, ya que si no lo hacemos el resultado nos daria un valor negativo.
+if (aniversarioArequipa < fechaActual) {
+    aniversarioArequipa.setFullYear(anioActual + 1);
+  }
 const tiempoFaltante = aniversarioArequipa - fechaActual;
 const diasFaltantes = Math.ceil(tiempoFaltante / (1000 * 60 * 60 * 24)); 
 
