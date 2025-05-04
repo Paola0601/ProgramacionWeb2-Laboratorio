0d212e1d93c38a2ceb4a125ec18f55c90248b9a9
Paola0601 <pmaytaqu@unsa.edu.pe>
2025-05-04 11:08:38 -0500

    Realizacion del ejercicio 1: listar todas las regiones con AJAX desde data.json,descargue le archivo data.json y use ajax

---
+++ Laboratorio4-tareaAjax/js/ejercicio1.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const listaRegiones=document.getElementById("Lista_de_regiones");
+    const peticion = new XMLHttpRequest();
+    peticion.open("GET", "data.json", true);
+
+    peticion.onload = function () {
+        if (peticion.status === 200) {
+            const datosDeDataJson = JSON.parse(peticion.responseText);
+            const regionesSet = new Set();
+
+             datosDeDataJson.forEach(regiones => {
+                if (regiones.region) {
+                regionesSet.add(regiones.region);
+                }
+              });
+
+            const regiones = Array.from(regionesSet).sort();
+
+              regiones.forEach(region => {
+              const li = document.createElement("li");
+              li.textContent = region;
+              listaRegiones.appendChild(li);
+             });
+         } else {
+              listaRegiones.innerHTML = "<li>Error al cargar los datos</li>";
+        }
+    };
+
+  peticion.onerror = function () {
+    listaRegiones.innerHTML = "<li>Error de conexión</li>";
+  };
+
+  peticion.send();
+});
