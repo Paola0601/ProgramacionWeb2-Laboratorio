94c5417b363131266186bc5b394ff684f59ea524
Paola0601 <pmaytaqu@unsa.edu.pe>
2025-05-04 13:38:21 -0500

    Voy a reutiliar mi codigo del ejercicio2

---
+++ Laboratorio4-tareaAjax/js/ejercicio3.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const listaRegiones=document.getElementById("casos_por_regiones");
+    const peticion = new XMLHttpRequest();
+    peticion.open("GET", "data.json", true);
+
+    peticion.onload = function () {
+        if (peticion.status === 200) {
+            const datosDeDataJson = JSON.parse(peticion.responseText);
+      
+            datosDeDataJson.forEach(regiones =>{
+              let nombre=regiones.region;
+              let fechaCantidad=regiones.confirmed;
+              let casosTotales=fechaCantidad.reduce((total,fecha) =>
+                  total+parseInt(fecha.value), 0);
+                            
+               const li = document.createElement("li");
+               li.textContent = nombre+": "+casosTotales;
+               listaRegiones.appendChild(li);
+             });
+         } else {
+              listaRegiones.innerHTML = "<li>Error al cargar los datos</li>";
+         }
+    };
+
+  peticion.onerror = function () {
+    listaRegiones.innerHTML = "<li>Error de conexión</li>";
+  };
+
+  peticion.send();
+});
