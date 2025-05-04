9cad068f00f5e3c09591132684c413a267315345
Paola0601 <pmaytaqu@unsa.edu.pe>
2025-05-04 13:47:23 -0500

    En el  ejercicio 3 implemente un arreglo, lo ordene de manera descendente y puse un for para que solo muestre los 10 primeros.

--- Laboratorio4-tareaAjax/js/ejercicio3.js
+++ Laboratorio4-tareaAjax/js/ejercicio3.js
@@ -1,22 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const listaRegiones=document.getElementById("casos_por_regiones");
+    const listaRegiones=document.getElementById("Top-10");
     const peticion = new XMLHttpRequest();
     peticion.open("GET", "data.json", true);
 
     peticion.onload = function () {
         if (peticion.status === 200) {
             const datosDeDataJson = JSON.parse(peticion.responseText);
-      
+            //En este ejercicio creamos un arreglo para almacenar los casos totales por region
+            let todosLosCasos=[];
             datosDeDataJson.forEach(regiones =>{
               let nombre=regiones.region;
               let fechaCantidad=regiones.confirmed;
               let casosTotales=fechaCantidad.reduce((total,fecha) =>
                   total+parseInt(fecha.value), 0);
-                            
-               const li = document.createElement("li");
-               li.textContent = nombre+": "+casosTotales;
-               listaRegiones.appendChild(li);
+              //Guardamos el nombre de la region y los casos totales en un objeto
+              todosLosCasos.push({nombre:nombre,total: casosTotales});
+           
              });
+             //Ordenamos el arreglo de forma decendente
+             todosLosCasos.sort((masCasos, menosCasos) => menosCasos.total - masCasos.total);
+             //Creamos un for para mostrar los primeros 10 casos totales en el HTML
+             for(let i=0;i<10;i++){
+             const li = document.createElement("li");
+             li.textContent = todosLosCasos[i].nombre + ": " + todosLosCasos[i].total;
+             listaRegiones.appendChild(li);}
          } else {
               listaRegiones.innerHTML = "<li>Error al cargar los datos</li>";
          }
