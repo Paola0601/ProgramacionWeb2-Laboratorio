28ff59ff05656b8ce8c5bc0b9a13a021300dc77e
Paola0601 <pmaytaqu@unsa.edu.pe>
2025-05-06 07:00:40 -0500

    Filtramos  la reiones que selecionams de acuerdo a las fechas y agrupamos casos diarion por fechas con um map

--- Laboratorio4-tareaAjax/js/ejercicio5.js
+++ Laboratorio4-tareaAjax/js/ejercicio5.js
@@ -1,3 +1,5 @@
+const { parse } = require("marked");
+
 google.charts.load("current", { packages: ["corechart"] });
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -6,7 +8,31 @@ document.addEventListener("DOMContentLoaded", function () {
    throw new Error("Error al cargar los datos del data.JSON");    
   }
    return respuesta.json();
- }
+}).then(datos =>{
+    
+    const regionesAcomparar=["Arequipa","Lima","Piura"];
+    const fechas=new Map();
+    regionesSeleccionadas.forEach(nombreRegion => {
+        const regionDatos=datos.find(regionesSeleccionadas=>regionesSeleccionadas.region===nombreRegion);
+      if(regionDatos){
+        regionDatos.confirmend.forEach(fechaRegion=>{
+            const fecha=fechaRegion.date;
+            const casos=parseInt(fechaRegion.value);      }
+           
+            if (!fechasMap.has(fecha)) {
+                fechasMap.set(fecha, { fecha });
+              }
+  
+              fechasMap.get(fecha)[nombreRegion] = valor;
+        });
+      }
+    });
+
+
+
+
+}
+ });
 
 
 
