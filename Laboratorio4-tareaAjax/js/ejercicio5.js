fb49a9e4352690f893c82caa417703ad02c69971
Paola0601 <pmaytaqu@unsa.edu.pe>
2025-05-06 06:34:08 -0500

    Hacemos la peticion de datos con fetch al archivo data.json

--- Laboratorio4-tareaAjax/js/ejercicio5.js
+++ Laboratorio4-tareaAjax/js/ejercicio5.js
@@ -1,5 +1,16 @@
 google.charts.load("current", { packages: ["corechart"] });
 
 document.addEventListener("DOMContentLoaded", function () {
- 
+ fetch("data.json").then(respuesta) => {
+  if(!respuesta.ok) {
+   throw new Error("Error al cargar los datos del data.JSON");    
+  }
+   return respuesta.json();
+ }
+
+
+
+
+
+
 });
