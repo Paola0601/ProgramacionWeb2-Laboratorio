64a2806f68756f496923231456e03036a12649eb
Paola0601 <pmaytaqu@unsa.edu.pe>
2025-05-11 13:08:40 -0500

    Implementamos el ejercicio 5

--- Laboratorio5-python/Ejercicio5.py
+++ Laboratorio5-python/Ejercicio5.py
@@ -1,2 +1,13 @@
+
 from interpreter import draw
-from chessPictures import queen
+from chessPictures import *
+
+#creamos una cuadriculaNegra
+cuadriculaNegra=square.negative()
+
+#Creamos una  fila de cuadrados que tenga blanco y negro
+filaBase = cuadriculaNegra.join(square)
+
+#Creamos una fila de 8 cuadrados, repitiendo filaBase 4 veces
+filaCompleta=filaBase.horizontalRepeat(4)
+draw(filaCompleta)
