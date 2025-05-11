3bd8312dd354742cc436de9acbdb9ac9b9565ecc
Paola0601 <pmaytaqu@unsa.edu.pe>
2025-05-11 14:24:19 -0500

    Ya salio el tablero completo pero aun hay inperfecciones

--- Laboratorio5-python/Ejercicio7.py
+++ Laboratorio5-python/Ejercicio7.py
@@ -25,8 +25,11 @@ filaBlanco=empiezaBlanco.horizontalRepeat(4)
 unionFilas = filaNegro.up(filaBlanco)
 #Repetimos la union de filas 2 veces
 mitadTablero = unionFilas.verticalRepeat(2)
-unimosFichasyTablero=unionFilas.up(mitadTablero.under(fichasCompletas))
-tableroFinal=unimosFichasyTablero.horizontalMirror()
+unimosFichasyTablero1=mitadTablero.under(fichasCompletas)
+mitadTablero=unionFilas.up(unimosFichasyTablero1)
+otraMitad=unimosFichasyTablero1.up(unionFilas).negative()
+
+tableroFinal=otraMitad.up(mitadTablero)
 draw(tableroFinal)
 
 
