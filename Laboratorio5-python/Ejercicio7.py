2d3e2d84e158e23a704698c5b7f32b6264507500
Paola0601 <pmaytaqu@unsa.edu.pe>
2025-05-11 13:48:27 -0500

    Implementamos las filas de peones

--- Laboratorio5-python/Ejercicio7.py
+++ Laboratorio5-python/Ejercicio7.py
@@ -1,2 +1,26 @@
 from interpreter import draw
 from chessPictures import *
+
+#creamos una cuadriculaNegra
+cuadriculaNegra=square.negative()
+#Creamos una fila de peones negros y blancos
+filaPeonesNegros=pawn.horizontalRepeat(8).negative()
+filaPeonesBlancos=pawn.horizontalRepeat(8)
+#Creamos una  fila de cuadrados que tenga blanco y negro
+empiezaNegro= cuadriculaNegra.join(square)
+
+#Creamos una fila de 8 cuadrados, repitiendo filaBase 4 veces
+filaNegro=empiezaNegro.horizontalRepeat(4)
+#Creamos una  fila de cuadrados que tenga negro y blanco
+empiezaBlanco = square.join(cuadriculaNegra)
+
+#Creamos una fila de 8 cuadrados, repitiendo filaBase 4 veces
+filaBlanco=empiezaBlanco.horizontalRepeat(4)
+#Juntamos las filas para hacer el tablero
+unionFilas = filaNegro.up(filaBlanco)
+#Repetimos la union de filas 2 veces
+mitadTablero = unionFilas.verticalRepeat(2)
+draw(filaPeonesNegros)
+
+
+#under
