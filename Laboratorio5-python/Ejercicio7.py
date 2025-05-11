87ee357d6c5bf90c0b3eb2792cf1af5e5ca5a68c
Paola0601 <pmaytaqu@unsa.edu.pe>
2025-05-11 14:10:38 -0500

    Unimos las fichas y el tablero

--- Laboratorio5-python/Ejercicio7.py
+++ Laboratorio5-python/Ejercicio7.py
@@ -25,7 +25,8 @@ filaBlanco=empiezaBlanco.horizontalRepeat(4)
 unionFilas = filaNegro.up(filaBlanco)
 #Repetimos la union de filas 2 veces
 mitadTablero = unionFilas.verticalRepeat(2)
-draw(fichasCompletas)
+unimosFichasyTablero=mitadTablero.under(fichasCompletas)
+draw(unimosFichasyTablero)
 
 
 #under
