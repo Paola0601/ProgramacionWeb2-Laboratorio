a371fe4caa828fb306004bcbe558e49a47560ccc
Paola0601 <pmaytaqu@unsa.edu.pe>
2025-05-11 13:18:21 -0500

    Implementamos en el ejercico 6 las dos lineas deltablero

--- Laboratorio5-python/Ejercicio6.py
+++ Laboratorio5-python/Ejercicio6.py
@@ -1,2 +1,17 @@
 from interpreter import draw
 from chessPictures import *
+#creamos una cuadriculaNegra
+cuadriculaNegra=square.negative()
+
+#Creamos una  fila de cuadrados que tenga blanco y negro
+empiezaNegro= cuadriculaNegra.join(square)
+
+#Creamos una fila de 8 cuadrados, repitiendo filaBase 4 veces
+filaNegro=empiezaNegro.horizontalRepeat(4)
+#Creamos una  fila de cuadrados que tenga negro y blanco
+empiezaBlanco = square.join(cuadriculaNegra)
+
+#Creamos una fila de 8 cuadrados, repitiendo filaBase 4 veces
+filaBlanco=empiezaBlanco.horizontalRepeat(4)
+
+draw(filaNegro)
